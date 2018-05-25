2b6382c9551a21a5c2923b0b0e83fa4042f5d284
Paulo Avelar <pauloavelar123@gmail.com>
2018-05-25 19:16:25 -0300

    Week: Reactive Forms Part 3

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -14,6 +14,29 @@ export class ContactComponent {
   feedback: Feedback;
   contactType = ContactType;
 
+  formErrors = { firstname: '', lastname: '', telnum: '', email: '' };
+
+  validationMessages = {
+    firstname: {
+      required: 'First Name is required.',
+      minlength: 'First Name must be at least 2 characters long.',
+      maxlength: 'FirstName cannot be more than 25 characters long.'
+    },
+    lastname: {
+      required: 'Last Name is required.',
+      minlength: 'Last Name must be at least 2 characters long.',
+      maxlength: 'Last Name cannot be more than 25 characters long.'
+    },
+    telnum: {
+      required: 'Tel. number is required.',
+      pattern: 'Tel. number must contain only numbers.'
+    },
+    email: {
+      required: 'Email is required.',
+      email: 'Email not in valid format.'
+    },
+  };
+
   constructor(
     private fb: FormBuilder
   ) {
@@ -22,14 +45,19 @@ export class ContactComponent {
 
   createForm() {
     this.feedbackForm = this.fb.group({
-      firstname: ['', Validators.required ],
-      lastname: ['', Validators.required ],
-      telnum: ['', Validators.required ],
-      email: ['', Validators.required ],
+      firstname: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25)] ],
+      lastname: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25)] ],
+      telnum: ['', [Validators.required, Validators.pattern] ],
+      email: ['', [Validators.required, Validators.email] ],
       agree: false,
       contacttype: 'None',
       message: ''
     });
+
+    this.feedbackForm.valueChanges
+      .subscribe(data => this.onValueChanged(data));
+
+    this.onValueChanged(); // (re)set validation messages now
   }
 
   onSubmit() {
@@ -43,6 +71,24 @@ export class ContactComponent {
       agree: false,
       contacttype: 'None',
       message: ''
+    });
+  }
+
+  onValueChanged(data?: any) {
+    if (!this.feedbackForm) return;
+
+    Object.keys(this.formErrors).forEach(field => {
+      // clear previous error message (if any)
+      this.formErrors[field] = '';
+
+      const control = this.feedbackForm.get(field);
+      if (control && control.dirty && !control.valid) {
+        const messages = this.validationMessages[field];
+
+        for (const key in control.errors) {
+          this.formErrors[field] += messages[key] + ' ';
+        }
+      }
     });
   }
 
